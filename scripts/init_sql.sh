af9e2e47eff107d0721592a447b93406371fd696
H37kouya <h37kouya@gmail.com>
2022-02-23 11:55:16 +0900

    refactor いらない変数の削除

--- scripts/init_sql.sh
+++ scripts/init_sql.sh
@@ -8,8 +8,6 @@ shopt -s expand_aliases
 alias dc='docker compose'
 
 # ファイルが存在する現在のディレクトリのパスを取得
-DIR=$(cd $(dirname $0); pwd)
-EMPLOYEES_SQL_DIR=${DIR}/../resources/test_db
 CONTAINER_NAME=db
 
 dc exec $CONTAINER_NAME /bin/sh -c "cd test_db && mysql -u root -ptester < employees.sql"
